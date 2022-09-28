a0c716b10bbe3732a014c4de08fc2125caa8dd39
usr <rlarlgus0206@naver.com>
2022-09-28 23:34:30 +0900

    Main

---
+++ src/main/java/com/S2E2/S2E2/S2E2Application.java
@@ -0,0 +1,13 @@
+package com.S2E2.S2E2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S2E2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S2E2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/S2E2/S2E2/S2E2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.S2E2.S2E2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S2E2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
